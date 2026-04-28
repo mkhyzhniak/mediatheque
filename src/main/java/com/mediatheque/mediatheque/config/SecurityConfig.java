cccff2af56b65f51c64665c3e570ac9ced267a64
masha <mashahizhnyak2004@gmail.com>
2026-04-28 17:03:29 +0300

    validation

--- src/main/java/com/mediatheque/mediatheque/config/SecurityConfig.java
+++ src/main/java/com/mediatheque/mediatheque/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/register", "/css/**").permitAll()
+                        .requestMatchers("/register", "/login", "/css/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
